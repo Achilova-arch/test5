d2ec96cf66cdcb31c19f1fcc62b0493fb8f5c65d
Achilova-arch <asalachilova96@gmail.com>
2025-07-18 11:57:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// let a=5; birinchi savol
+// let b=2;
+// alert (5%2);
+
+// let math=Math.random()*9+1; ikkinchi savol
+// alert ( Math.trunc (math));
+
+// let son=12.510;            uchinchi savol
+// alert (Math.trunc(son))
+
+// let matn= "MarsItschool";        tortinchi savol
+// alert (matn.length);
+
+// let text="Mars it school";          beshinchi savol
+// for (let i = 0; i< 10; i++) {
+//   console.log(text);   
+// }
+
+// let harfla=["a","b","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"]     oltinchi savol
+// console.log(harfla[0]+harfla[17]+harfla[0]+harfla[10]);
+
+// let yosh = prompt("Necha yoshda siz");             yetinchi savol
+// if (yosh > 18) {
+    // alert ("Siz balog'ot yoshga yetgansiz");
+// }else if(yosh == 18){
+    // alert ("balog'ot yoshi muborak");
+// }else{
+    // alert("ERROR");
+// }
+
+// let ism=prompt("Ismingizni kiriting");      sakizinchi savol
+// console.log(ism);
+
+
+// let ism=prompt("Ism kiriting");                        toqizinchi savol
+// let yosh=prompt("Yosh kiriting");
+// console.log(document.getElementById("yosh"));
+// console.log(document.getElementById ("ism"));
+
+// let sonlar=[1,2,3,4,5,6,7,8,9,10];                 oninchi savol
+// console.log(sonlar%2);
